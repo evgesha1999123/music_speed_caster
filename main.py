40cc854796b11c28b3e20b976a87d5fb319c6db5
evgesha1999123 <62473546+evgesha1999123@users.noreply.github.com>
2024-06-15 14:21:26 +0300

    0 architecture

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import sys
 import re
 import time
 from mutagen.mp3 import MP3
-
-print(">>>>>>>>>>>>>>>>>>>>>>>>>" + AudioSegment.ffmpeg)
+from CTkListbox import *
+import eyed3
 
 # Set the theme and color options
 ctk.set_appearance_mode("dark")  # Modes: system (default), light, dark
@@ -46,6 +46,9 @@ class App(ctk.CTk):
         self.is_pause = False
         self.current_time = 0
 
+        self.playlist_list = []
+        self.last_index_of_playlist = 0
+
         self.ms_gothic_font = ctk.CTkFont(family = "@MS Gothic", size = 14)
 
         self.image_open_file = Image.open('resources/icon_open_file.png')
@@ -58,6 +61,9 @@ class App(ctk.CTk):
         self.image_previous_track = Image.open('resources/icon_previous_track.png')
         self.image_rewind_forward = Image.open('resources/icon_rewind_forward.png')
         self.image_rewind_back = Image.open('resources/icon_rewind_back.png')
+        self.image_show_playlist = Image.open('resources/icon_show_playlist.png')
+        self.image_hide_playlist = Image.open('resources/icon_hide_playlist.png')
+        self.image_unknown_almub = Image.open('resources/unknown_album.png')
 
         self.photo_open_file = ImageTk.PhotoImage(self.image_open_file)
         self.photo_play_button = ImageTk.PhotoImage(self.image_play_button)
@@ -69,10 +75,30 @@ class App(ctk.CTk):
         self.photo_previous_track = ImageTk.PhotoImage(self.image_previous_track)
         self.photo_rewind_forward = ImageTk.PhotoImage(self.image_rewind_forward)
         self.photo_rewind_back = ImageTk.PhotoImage(self.image_rewind_back)
+        self.photo_show_playlist = ImageTk.PhotoImage(self.image_show_playlist)
+        self.photo_hide_playlist = ImageTk.PhotoImage(self.image_hide_playlist)
+
+
+        self.img = self.image_unknown_almub.resize((400, 400), )
+
+        self.title_image = ImageTk.PhotoImage(image = self.img)
+
+        self.canvas = ctk.CTkLabel(self, text = None, fg_color = 'gray', image = self.title_image); self.canvas.place(relx = 0.4, rely = 0.2)
+
+        self.img.close()
+
 
         self.pan_button_frame = ctk.CTkFrame(self, width = self._current_width, height = 100, fg_color = 'black', bg_color = 'black'); self.pan_button_frame.pack(side = tk.BOTTOM, fill = tk.BOTH, expand = False)
 
         self.pan_status_frame = ctk.CTkFrame(self, width = self._current_width, height = 100, fg_color = 'dark orange', border_color = 'black', border_width = 10); self.pan_status_frame.pack(side = tk.BOTTOM, fill = tk.BOTH, expand = False)
+
+        self.pan_playlist = ctk.CTkFrame(self, width = 800, height = 800, fg_color = 'black', bg_color = 'black'); self.pan_playlist.pack(side = tk.RIGHT, fill = Y)
+
+        self.button_hide_playlist = ctk.CTkButton(self.pan_playlist, text = None, width = 50, height = 50, image = self.photo_hide_playlist, command = self.hide_playlist); self.button_hide_playlist.place(relx = 0.8, rely = 0.01)
+
+        self.button_show_playlist = ctk.CTkButton(self, text = None, width = 50, height = 50, image = self.photo_show_playlist, command = self.show_playlist)
+
+        self.button_add_to_playlist = ctk.CTkButton(self.pan_playlist, text = 'ADD', command = self.add_to_playlist); self.button_add_to_playlist.pack()
 
         self.filename_label = ctk.CTkLabel(self.pan_status_frame, text = 'Waiting for opening file', text_color = 'black', font = self.ms_gothic_font)
         self.filename_label.place(relx = 0.4, rely = 0.1)
@@ -115,9 +141,8 @@ class App(ctk.CTk):
         
         self.button_previous_track.place(relx = 0.25, rely = 0.35, anchor = W)
 
-
         self.change_speed_button = ctk.CTkButton(self.pan_button_frame, text = None, width = 50, height = 50,
-                                                image = self.photo_change_speed_button, command = self.init_speed_changer_thread)
+                                                 image = self.photo_change_speed_button, command = self.init_speed_changer_thread)
         
         self.change_speed_button.place(relx = 0.8, rely = 0.35, anchor = W)
 
@@ -137,10 +162,140 @@ class App(ctk.CTk):
         self.settings_button.place(relx = 0.01, rely = 0.01)
 
         self.progressbar = ctk.CTkProgressBar(self.pan_status_frame, mode = "indeterminate")
+
+        self.playlist = CTkListbox(self.pan_playlist, width = 300, height = 400, bg_color = 'black', command = self.select_track)
+        self.playlist.pack(side = 'right')
+        
         
 
 
-#----------------------------------------------------------------------------Settings window
+#----------------------------------------------------------------------------Playlist
+
+
+
+    def add_to_playlist(self):
+        playlist_tuple_abspath = filedialog.askopenfilenames(title="open audiofile", filetypes=[("audio", "*.mp3"), ("audio", "*.wav")])
+        playlist_to_add = list(playlist_tuple_abspath)
+        step_value = 0
+        index = 0
+
+
+        # if self.playlist_list.__len__() >= 1:       
+        #     if len(playlist_to_add) > len(self.playlist_list) : greater_list = playlist_to_add; smaller_list = self.playlist_list
+        #     else : greater_list = self.playlist_list; smaller_list = playlist_to_add
+
+        #     while step_value < smaller_list.__len__():
+        #         if greater_list[index] == smaller_list[step_value] : 
+        #            smaller_list[step_value] = 0
+        #         if index < len(greater_list) - 1:
+        #             index += 1
+        #         else:
+        #             step_value += 1
+        #             index = 0
+
+
+        #     index = 0
+            
+        #     playlist_to_add = smaller_list
+
+        #     while 0 in playlist_to_add : playlist_to_add.remove(0)
+            
+                
+        # index = 0
+
+        while index < playlist_to_add.__len__(): 
+            self.playlist_list.append(playlist_to_add[index])                                                       #Добавление в глобальный список с треками
+            index += 1
+            self.last_index_of_playlist += 1
+        index = 0
+
+        while index < self.playlist_list.__len__() :
+            self.playlist.insert(index, os.path.basename(self.playlist_list[index]))                                #Добавление в плейлист
+            index += 1  
+
+
+    def hide_playlist(self):
+        self.pan_playlist.pack_forget()
+        self.button_show_playlist.place(relx = 0.95, rely = 0.01)
+
+    def show_playlist(self):
+        self.pan_playlist.pack(side = tk.RIGHT, fill = Y)
+        self.button_show_playlist.place_forget()
+
+
+    
+    def select_track(self, selected_track):
+        print(f'{selected_track} {[self.playlist.curselection()]} {self.playlist_list[self.playlist.curselection()]}')
+        self.magic_button_pressed = False
+        self.is_path_valid = True
+        self.first_file_start = True
+        self._filepath_ = self.playlist_list[self.playlist.curselection()]
+
+        if mixer.music.get_busy() == True : self.stop_music()
+
+        self.img.close()
+
+        self.parse_title_image_from_file()
+
+        self.current_song = mixer.Sound(self._filepath_)
+        self.filename_label.configure(text = f'Now playing : {selected_track}')
+        self.song_lenght = mixer.Sound.get_length(self.current_song)
+        self.song_lenght = round(self.song_lenght, 1)
+
+        #if self._filepath_ != '' and self._filepath_ != None : self.stop_music()
+
+        self.time_slider.pack()
+        self.time_slider.place(relx = 0.5, rely = 0.5, anchor = CENTER)
+        self.time_slider.configure(from_ = 0, to = self.song_lenght)
+        self.time_slider.set(0)
+        self.song_lenght_label.configure(text = '00:00 / 00:00')
+        self.song_lenght_label.pack()
+        self.song_lenght_label.place(relx = 0.48, rely = 0.55)
+        self.change_speed_button.configure(state = 'normal')
+
+        self.pause_and_play_button_click_event()
+
+
+
+    def next_track_click_event(self):
+        selected_element = self.playlist_list[self.playlist.curselection()]
+        if selected_element == self.playlist.size() : return
+        else : 
+            self.stop_music()
+            self.pause_and_play_button_click_event()
+
+    def previous_track_click_event(self):
+        ...
+
+
+    def parse_title_image_from_file(self):
+        audio_file = eyed3.load(self._filepath_)
+        try:
+            album_name = audio_file.tag.album
+            artist_name = audio_file.tag.artist
+            for image in audio_file.tag.images:
+                image_file = open("cached images/{0} - {1}({2}).jpg".format(artist_name, album_name, image.picture_type), "wb")
+                image_name = ('cached images/{0} - {1}({2}).jpg'.format(artist_name, album_name, image.picture_type))
+                image_file.write(image.image_data)
+                image_file.close()
+            
+            print(f'>>>>>>>>>>>>IMAGE FILE ::: {image_name}')
+            
+            self.img = Image.open(image_name)
+
+            self.img = self.img.resize((400, 400), )
+
+            self.title_image = ImageTk.PhotoImage(image = self.img)
+
+            self.canvas.configure(image = self.title_image)
+        except: 
+            self.img = self.image_unknown_almub
+
+            self.img = self.img.resize((400, 400), )
+
+            self.title_image = ImageTk.PhotoImage(image = self.img)
+
+            self.canvas.configure(image = self.title_image)
 
 
 
@@ -148,6 +303,12 @@ class App(ctk.CTk):
         if re.match("^\d+?\.\d+?$", value) is None:
             return value.isdigit()
         return True
+
+
+
+#-------------------------------------------------------------------------------- Settings Window
+
+
 
     def settings_click_event(self):
         self.settings_button.configure(state = 'disabled')
@@ -183,16 +344,31 @@ class App(ctk.CTk):
         self.speed_multiplier_label_value = ctk.CTkLabel(self.settings_window, text = self.speed_multiplier); self.speed_multiplier_label_value.pack()
         self.speed_multiplier_label_value.place(relx = 0.3, rely = 0.37)
 
+
+
     def enable_settings_button(self):
         self.settings_button.configure(state = 'normal')
         self.settings_window.destroy()
 
+
+
     def button_save_config_event(self):
         self.speed_multiplier = self.speed_multiplier_label_value._text #<---set global value
-        self.settings_button.configure(state = 'normal')
-        self.settings_window.destroy()
+        if self.entry_speed_multiplier.get() != None and self.entry_speed_multiplier.get() != '' and self.to_number(self.entry_speed_multiplier.get()) == True and (float(self.entry_speed_multiplier.get()) > 0.1 and float(self.entry_speed_multiplier.get()) <= 4):
+            self.speed_multiplier = float(self.entry_speed_multiplier.get())
+            print(self.speed_multiplier)
+            self.settings_button.configure(state = 'normal')
+            self.change_speed_button.configure(state = 'normal')
+            self.init_speed_changer_thread()
+            self.settings_window.destroy()
+        else : 
+            self.entry_speed_multiplier.configure(border_color = 'red')
+            return
+
+
 
     def slider_set_track_speed_event(self, event):
+        self.entry_speed_multiplier.configure(border_color = 'gray')
         speed_multiplier_value = self.settings_slider_set_track_speed.get()
         speed_multiplier_value = round(speed_multiplier_value, 2)
         self.speed_multiplier_label_value.configure(text = speed_multiplier_value)
@@ -200,15 +376,26 @@ class App(ctk.CTk):
         self.entry_speed_multiplier.delete(0, 'end')
         self.entry_speed_multiplier.insert(1, self.speed_multiplier_label_value._text)
 
+
+
     def parse_speed_player_value(self, event):
         is_number = self.to_number(self.entry_speed_multiplier.get())
         if is_number == True : float_entry_speed_multiplier_value = float(self.entry_speed_multiplier.get())
-        else : return
+        else :
+            self.entry_speed_multiplier.configure(border_color = 'red')
+            return
 
         if float_entry_speed_multiplier_value >= 0.1 and float_entry_speed_multiplier_value <= 3:
+            self.entry_speed_multiplier.configure(border_color = 'gray')
             self.speed_multiplier_label_value.configure(text = float_entry_speed_multiplier_value)
             self.settings_slider_set_track_speed.set(float_entry_speed_multiplier_value)
-        else : return
+        else :
+            self.entry_speed_multiplier.configure(border_color = 'red')
+            return
+
+
+
+#------------------------------------------------------------------------------------------- 
 
 
 
@@ -233,14 +420,6 @@ class App(ctk.CTk):
             else:
                 mixer.music.play(loops = 0, start = int(self.time_slider.get()) - 3)
                 self.current_time -= 3
-
-
-
-    def next_track_click_event(self):
-        ...
-
-    def previous_track_click_event(self):
-        ...
 
 
 
@@ -309,26 +488,23 @@ class App(ctk.CTk):
                 mixer.music.unpause()
                 self.pause_and_play_button.configure(image = self.photo_pause_button)
                 self.is_pause = False
-                #self.watch_track_progress()
             
 
     def time_slider_event_release(self, event):
-        if self.is_pause == False and self.is_stop == False:
-            mixer.music.load(self._filepath_)
-            mixer.music.play(loops = 0, start = int(self.time_slider.get()))
-            self.current_time = int(self.time_slider.get())
+        if self._filepath_ == None or self._filepath_ == '' : return
+        else :
+            if self.is_pause != True and self.is_stop != True:
+                mixer.music.play(loops = 0, start = int(self.time_slider.get()))
+                self.current_time = int(self.time_slider.get())
         
         
 
     def volume_slider_event(self, event):
         mixer.music.set_volume(self.volume_slider.get())
-        print(mixer.music.get_volume())
 
     def change_speed(self):
-        if self._filepath_ == None or self._filepath_ == '' : return
-        else :
             self.change_speed_button.configure(state = 'disabled')
-            n_song = AudioSegment.from_file(self._filepath_)
+            n_song = AudioSegment.from_file(self.playlist_list[self.playlist.curselection()])
             # Manually override the frame_rate. This tells the computer how many
             # samples to play per second
             sound_with_altered_frame_rate = n_song._spawn(n_song.raw_data, overrides={
@@ -341,25 +517,25 @@ class App(ctk.CTk):
             try:
                 sound_with_altered_frame_rate.export('tmp/tmp_audiofile.mp3', format = 'mp3')
                 self.magic_button_pressed = True
-                msg = "Магия работает, используйте на здоровье =)"
                 self.progressbar.place_forget()
-                mb.showinfo("Информация", msg)
                 self.stop_music()
+                self.pause_and_play_button_click_event()
             except PermissionError:
                 mixer.quit()
                 sound_with_altered_frame_rate.export('tmp/tmp_audiofile.mp3', format = 'mp3')
                 self.magic_button_pressed = True
-                msg = "Магия работает, используйте на здоровье =)"
                 self.progressbar.place_forget()
-                mb.showinfo("Информация", msg)
                 mixer.init()
+                self.stop_music()
+                self.pause_and_play_button_click_event()
 
     
     def init_speed_changer_thread(self):
-        self.alt_speed_thread = threading.Thread(target = self.change_speed, daemon = True)
-        self.alt_speed_thread.start()
-        self.progressbar.place(relx = 0.8, rely = 0.2)
-        self.progressbar.start()
+        if self._filepath_ != None and self._filepath_ != '' :
+            self.alt_speed_thread = threading.Thread(target = self.change_speed, daemon = True)
+            self.alt_speed_thread.start()
+            self.progressbar.place(relx = 0.8, rely = 0.2)
+            self.progressbar.start()
 
     def close_window_event(self):
         try:
@@ -399,11 +575,8 @@ class App(ctk.CTk):
 
         self.time_slider.set(int(current_time))
 
-        print(f'get_pos = {player_time} GLOBAL self.current_time = {self.current_time}')
-        print(f'{int(current_time)} {int(self.song_lenght)}')
-
         self.song_lenght_label.configure(text = string_timebar)
-        self.song_lenght_label.after(1000, self.watch_track_progress)
+        self.song_lenght_label.after(450, self.watch_track_progress)
 
                 
 
@@ -414,5 +587,4 @@ if __name__ == "__main__":
     pygame.init()
     mixer.pre_init(frequency=44100, size = -16, channels=2, buffer=512, devicename=None, allowedchanges=0)
     mixer.init(frequency=44100, size = -16, channels=2, buffer=512, devicename=None, allowedchanges=0)
-    print(pygame.mixer.get_init())
     app.mainloop()
